c5fc6eb77050344352aaee25f6d503decbbfef0b
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-10-20 22:18:50 +0530

    sighup

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,17 +1,36 @@
+import {signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+import app from '../utils/firebase'
+import { useNavigate } from "react-router-dom";
+
+const auth = getAuth(app);
 
 
 const SignUp = () => {
 
-    const signUpUser = async() => {
+const navigate = useNavigate();
 
-        
+    const handleGoogleSignUp = async () => {
+      
+      const provider = new GoogleAuthProvider();
+      
+      try {
+        const result = await signInWithPopup(auth, provider);
+  
+        const user = result.user;
+        console.log('The user : ', user);
+        navigate('/')
+      } catch (err: any) {
+      
+         console.log(err.message)
+      }
     }
+
 
   return (
     <div className=" mx-auto my-96 bg-opacity-35">
       <div className="lg:w-[18rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:px-6 md:pt-0">
         <button className="-2 mt-8 flex items-center justify-center rounded-md border px-4 py-1 outline-none ring-gray-400 text-white ring-offset-2 transition focus:ring-2 hover:border-transparent quicksand-bold hover:bg-black hover:text-yellow-400"
-           onClick={signUpUser}
+           onClick={handleGoogleSignUp}
         >
           <img
             className="mr-2 h-5 "
--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+import { FIREBASE_API_KEY } from './Firebase_Api_Key'  
+
+console.log('the key', FIREBASE_API_KEY)
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "clever-ai-5bc0c.firebaseapp.com",
-  projectId: "clever-ai-5bc0c",
-  storageBucket: "clever-ai-5bc0c.appspot.com",
-  messagingSenderId: "1071752747014",
-  appId: "1:1071752747014:web:e11009f9f2b20d6264cb87"
-};
+    apiKey: FIREBASE_API_KEY,
+    authDomain: "clever-ai-5bc0c.firebaseapp.com",
+    projectId: "clever-ai-5bc0c",
+    storageBucket: "clever-ai-5bc0c.appspot.com",
+    messagingSenderId: "1071752747014",
+    appId: "1:1071752747014:web:e11009f9f2b20d6264cb87"
+  };
+ 
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
+export default app;
