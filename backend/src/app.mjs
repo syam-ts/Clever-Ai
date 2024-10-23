5c97b80a0cff3900144d593c7d325bd0699d02b8
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-10-23 14:12:01 +0530

    packages bug fix

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser'
-import getAiRouter from './routes/getAiRoute.ts'
+import getAiRouter from './routes/getAiRoute.js'
 import cors from 'cors'
 
 const app = express();
